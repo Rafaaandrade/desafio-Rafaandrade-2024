de2151c4ab64ef78b9d87f9216717c6e04d64ed1
Rafaaandrade <rafaandradejf@gmail.com>
2024-09-11 21:04:27 -0300

    feat: Avanço nas funções / bug: resolver lógica para passar no último teste

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -21,8 +21,9 @@ class RecintosZoo {
   }
 
   analisaRecintos(animal, quantidade) {
-    let animalRecebido = animal.toLowerCase();
+    const animalRecebido = animal.toLowerCase();
     const tipoAnimal = this.animais[animalRecebido];
+
     if (!tipoAnimal) {
       return { erro: "Animal inválido" };
     }
@@ -30,6 +31,60 @@ class RecintosZoo {
     if (quantidade <= 0 || Number.isNaN(quantidade)) {
       return { erro: "Quantidade inválida" };
     }
+
+    const recintosViaveis = this.recintos.filter((recinto) => {
+      const bioma = recinto.bioma.toLowerCase();
+      const animalBiomas = [tipoAnimal.bioma];
+
+      if (tipoAnimal.alternativa) {
+        animalBiomas.push(tipoAnimal.alternativa);
+      }
+
+      if (!animalBiomas.includes(bioma)) {
+        return false;
+      }
+
+      // Verificar regras específicas
+      const animaisExistentes = recinto.animaisExistentes.map((a) =>
+        a.toLowerCase()
+      );
+      const jaTemAnimais = animaisExistentes.length > 0;
+      const precisaEspacoExtra = jaTemAnimais ? 1 : 0;
+
+      if (animalRecebido === "hipopotamo" && bioma !== "savana e rio") {
+        return false;
+      }
+
+      if (animalRecebido === "macaco" && !jaTemAnimais) {
+        return false;
+      }
+
+      if (
+        animalRecebido !== "hipopotamo" &&
+        animaisExistentes.some((a) => a !== animalRecebido)
+      ) {
+        return false;
+      }
+
+      // const espacoNecessario =
+      //   tipoAnimal.tamanho * quantidade + precisaEspacoExtra;
+      return (
+        recinto.adicionarAnimal(tipoAnimal, quantidade) && recinto.espacoLivre()
+      );
+    });
+
+    if (recintosViaveis.length === 0) {
+      return { erro: "Não há recinto viável" };
+    }
+
+    const resultado = recintosViaveis.map((recinto) => {
+      return `Recinto ${recinto.numero} (espaço livre: ${
+        recinto.espacoLivre() - tipoAnimal.tamanho * quantidade
+      } total: ${recinto.tamanhoTotal})`;
+    });
+
+    console.log("resultado", recintosViaveis);
+    return { recintosViaveis: resultado };
   }
 }
 
